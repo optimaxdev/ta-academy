7f535a4a6c3d7a1c65dd7f84ecb7f7e28ed22ec3
Mikhail Lebedev <Lmikhail163@gmail.com>
2023-02-20 21:41:07 +0400

    feet: fixed mistake

--- lesson_4/tests/magazinesEvent.test.ts
+++ lesson_4/tests/magazinesEvent.test.ts
@@ -1,9 +1,30 @@
 import { expect, test } from '@playwright/test';
 import { DataLayer } from '@Utils/dataLayer';
+import { forEachSeries } from 'p-iteration';
 
 test.describe('get all magazines, check that there are 7 of them and catch an event on every click', () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
+  });
+
+  test('check event in dataLayer when scrolling to the featured block', async ({ page }) => {
+    const dataLayer = new DataLayer(page);
+  
+    const expectedEvent = {
+      event: 'HPInteraction',
+      eventAction: 'Magazines',
+      eventCategory: 'HP - D',
+      eventLabel: 'Visible',
+    };
+  
+    await page.waitForTimeout(2000);
+  
+    await page.mouse.wheel(0, 4700);
+    const [event] = await dataLayer.waitForDataLayer({
+      event: 'HPInteraction',
+      eventAction: 'Magazines',
+    });
+    expect(event).toStrictEqual(expectedEvent);
   });
 
   test('get all magazines, check that there are 7 of them', async ({ page }) => {
@@ -28,10 +49,10 @@ test.describe('get all magazines, check that there are 7 of them and catch an ev
       '//*[@id="react-root"]/div[5]/section/ul/li'
     );
 
-    magazines.forEach(async (magazine) => {
+    await forEachSeries(magazines, async (magazine) => {
       await page.evaluate(() => window.dataLayer = []);
 
-      await magazine?.click();
+      await magazine.click();
 
       const [event] = await dataLayer.waitForDataLayer({
         event: 'HPInteraction',
