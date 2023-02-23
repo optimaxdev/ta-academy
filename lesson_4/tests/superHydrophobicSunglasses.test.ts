3128f83286668ceaa42554ffc54097e57e9900e7
Dmitry Shilovsky <wppbbh@gmail.com>
2023-02-23 04:24:46 +0400

    rework test

--- lesson_4/tests/superHydrophobicSunglasses.test.ts
+++ lesson_4/tests/superHydrophobicSunglasses.test.ts
@@ -2,7 +2,6 @@ import { expect, test } from '@Test';
 import { DataLayer } from '@Utils/dataLayer';
 
 test.describe.only('PDPInteraction events', () => {
-    let dataLayer: DataLayer;
     const expectedEvent = {
         event: 'PDPInteraction',
         eventAction: 'Sun Lens Funnel - Step 4: Coating',
@@ -10,17 +9,15 @@ test.describe.only('PDPInteraction events', () => {
     };
 
     test.beforeEach(async ({ page, homePage, productPage, categoryPage }) => {
-        dataLayer = new DataLayer(page);
-
-        await homePage.open(); //Открываем хомепэйж
-        await homePage.Header.buttonSunglassesClick(); //кликаем на кнопку санглас
-        await categoryPage.firstProductClick(); // выбираем первое очко
-        await productPage.buttonSelecetLensesClick(); // клик выбрать линзу
+        await homePage.open();
+        await homePage.Header.buttonSunglassesClick();
+        await categoryPage.firstProductClick();
+        await productPage.buttonSelecetLensesClick();
         await productPage.Wizard.buttonNonPrescriptionClick();
         await productPage.Wizard.buttonContinueClick();
         await productPage.Wizard.buttonContinueClick();
     });
-    test('should fire after adding coating and removing it', async ({ page, productPage }) => {
+    test('should fire after adding coating and removing it', async ({ dataLayer, productPage }) => {
         const verifyEvent = dataLayer.createEventVerifier(expectedEvent);
 
         await productPage.Wizard.buttonContinueClick();
