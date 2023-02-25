8ef4552db0776373e184c35610a7c306a57a3edf
alexandra_shmoylova@yahoo.com <alexandra_shmoylova@yahoo.com>
2023-02-25 13:04:55 +0400

    Add Super Hydrophobic test

--- lesson_4/tests/selectLensesEvent.test.ts
+++ lesson_4/tests/selectLensesEvent.test.ts
@@ -1,54 +1,65 @@
 import { expect, test } from '@playwright/test';
 import { DataLayer } from '@Utils/dataLayer';
+import { waitFor } from '@Utils/waitFor';
 
 test.describe.configure({ mode: 'serial' });
 
 test.describe('check sunglasses event', async () => {
   test.beforeEach(async ({ page }) => {
     await page.goto('/');
-    // await page.waitForURL(
-    //   'https://ta-0000-gusa-desktop.gusadev.com',
-    //   { waitUntil: 'domcontentloaded' }
-    // );
-  });
-  test('catch event after steps', async ({ page }) => {
-    const dataLayer = new DataLayer(page);
 
+    const sunglassesButton = await page.$(
+      '//nav//a[contains(., "Sunglasses")]'
+    );
+    await sunglassesButton?.click();
+    await page.waitForTimeout(10000);
+
+    const sunglassesItem = await page.$$(
+      '//ul[contains(@class, "categoryRows__list")]//a'
+    );
+
+    await sunglassesItem[0]?.click();
+    await page.waitForTimeout(10000);
+
+    const selectLensesButton = await page.$(
+      '//button[@aria-label="choose lenses"]'
+    );
+    await selectLensesButton?.click();
+    await page.waitForTimeout(10000);
+
+    const usageGlassesbutton = await page.$(
+      '//div[@role="button" and contains(., "Non-prescription")]'
+    );
+    await usageGlassesbutton?.click();
+    await page.waitForTimeout(5000);
+
+    const continueBtn = await page.$(
+      '//button[contains(., "Continue")]'
+    );
+    await continueBtn?.click();
+    await page.waitForTimeout(5000);
+    const continueBtn2 = await page.$(
+      '//button[contains(., "Continue")]'
+    );
+    await continueBtn2?.click();
+    await page.waitForTimeout(10000);
+  });
+
+  test('catch "No Coating Added" event', async ({
+    page,
+  }) => {
+    const dataLayer = new DataLayer(page);
     const expectedEvent = {
       event: 'PDPInteraction',
       eventCategory: 'PDP - D',
       eventAction: 'Sun Lens Funnel - Step 4: Coating',
       eventLabel: 'No Coating Added',
     };
-
-    const sunglassesButton = await page.$(
-      '//nav//a[contains(., "Sunglasses")]'
-    );
-
-    await sunglassesButton?.click();
-
-    const sunglassesItem = await page.$(
-      '//ul[contains(@class, "categoryRows__list")]/a[0]'
-    );
-    await sunglassesItem?.click();
-
-    const selectLensesButton = await page.$(
-      '//button[contains(@aria-label, "choose lenses")]'
-    );
-    await selectLensesButton?.click();
-
-    const usageGlassesbutton = await page.$(
-      '//div[contains(@role, "button")][0]'
-    );
-    await usageGlassesbutton?.click();
-
-    const continueBtn = await page.$(
+    const continueBtn3 = await page.$(
       '//button[contains(., "Continue")]'
     );
-    await continueBtn?.click();
-    await continueBtn?.click();
-    await continueBtn?.click();
-
+    await continueBtn3?.click();
+    await page.waitForTimeout(10000);
     const [event] = await dataLayer.waitForDataLayer({
       event: 'PDPInteraction',
       eventAction: 'Sun Lens Funnel - Step 4: Coating',
@@ -56,5 +67,79 @@ test.describe('check sunglasses event', async () => {
     });
 
     expect(event).toStrictEqual(expectedEvent);
+  });
+
+  test('catch event "Super Hydrophobic - Add"', async ({
+    page,
+  }) => {
+    const dataLayer = new DataLayer(page);
+    const hydrophobicBtn = await page.$(
+      'input[value="Super Hydrophobic"]'
+    );
+    await hydrophobicBtn?.click();
+    await page.waitForTimeout(10000);
+    const continueBtn3 = await page.$(
+      '//button[contains(., "Continue")]'
+    );
+    await continueBtn3?.click();
+    await page.waitForTimeout(10000);
+    const expectedHydraAddEvent = {
+      event: 'PDPInteraction',
+      eventCategory: 'PDP - D',
+      eventAction: 'Sun Lens Funnel - Step 4: Coating',
+      eventLabel: 'Super Hydrophobic - Add',
+    };
+    const [event] = await dataLayer.waitForDataLayer({
+      event: 'PDPInteraction',
+      eventAction: 'Sun Lens Funnel - Step 4: Coating',
+      eventLabel: 'Super Hydrophobic - Add',
+    });
+    expect(event).toStrictEqual(expectedHydraAddEvent);
+  });
+
+  test('catch event "Super Hydrophobic - Remove"', async ({
+    page,
+  }) => {
+    const dataLayer = new DataLayer(page);
+
+    const hydrophobicBtn = await page.$(
+      'input[value="Super Hydrophobic"]'
+    );
+    await hydrophobicBtn?.click();
+    await page.waitForTimeout(10000);
+    const continueBtn3 = await page.$(
+      '//button[contains(., "Continue")]'
+    );
+    await continueBtn3?.click();
+
+    const backButton = await page.$$(
+      '//button[contains(., "Back")]'
+    );
+    await backButton[1]?.click();
+    await page.waitForTimeout(5000);
+
+    const hydrophobicBtn2 = await page.$(
+      'input[value="Super Hydrophobic"]'
+    );
+    await hydrophobicBtn2?.click();
+    await page.waitForTimeout(10000);
+
+    const continueBtn4 = await page.$(
+      '//button[contains(., "Continue")]'
+    );
+    await continueBtn4?.click();
+
+    const expectedHydraAddEvent = {
+      event: 'PDPInteraction',
+      eventCategory: 'PDP - D',
+      eventAction: 'Sun Lens Funnel - Step 4: Coating',
+      eventLabel: 'Super Hydrophobic - Remove',
+    };
+    const [event] = await dataLayer.waitForDataLayer({
+      event: 'PDPInteraction',
+      eventAction: 'Sun Lens Funnel - Step 4: Coating',
+      eventLabel: 'Super Hydrophobic - Remove',
+    });
+    expect(event).toStrictEqual(expectedHydraAddEvent);
   });
 });
