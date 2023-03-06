10a7419dcb0ce70c9b08db8e6e6cd6315029c31e
Dmitry Shilovsky <wppbbh@gmail.com>
2023-03-06 09:56:57 +0400

    Homework6 done

--- lesson_4/src/customTest.ts
+++ lesson_4/src/customTest.ts
@@ -2,13 +2,11 @@ import { test as base, expect } from '@playwright/test';
 import { DataLayer } from '@Utils/dataLayer';
 import { HomePage } from '@Pages/homePage';
 import { CategoryPage } from '@Pages/categoryPage';
-import { MyAccountPage } from '@Pages/myAccountPage';
 
 type Options = {
     dataLayer: DataLayer;
     homePage: HomePage;
     categoryPage: CategoryPage;
-    myAccountPage: MyAccountPage;
 };
 
 const test = base.extend<Options>({
@@ -20,9 +18,6 @@ const test = base.extend<Options>({
     },
     categoryPage: async ({ page }, use) => {
         await use(new CategoryPage(page));
-    },
-    myAccountPage: async ({ page }, use) => {
-        await use(new MyAccountPage(page));
     },
 });
 
--- lesson_4/tests/newAccount.test.ts
+++ lesson_4/tests/newAccount.test.ts
@@ -2,61 +2,23 @@ import { expect, test } from '@Test';
 import faker from 'faker';
 
 test.describe('create new account', () => {
-    test('create new account, change data and check data', async ({ homePage, myAccountPage }) => {
+    const email = faker.internet.email();
+    const accountData = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        password: 'Password123',
+    };
+
+    test('create new account via tooltip', async ({ homePage }) => {
         await homePage.open();
-
         const header = homePage.Header;
-        const myAccountSideBar = myAccountPage.myAccountSideBar;
-        const myAccountContent = myAccountPage.myAccountContent;
+        await header.clickAccountTooltip();
+        await header.clickCreateAccount();
 
-        const email = faker.internet.email();
-        const accountData = {
-            firstName: faker.name.firstName(),
-            lastName: faker.name.lastName(),
-            password: faker.internet.password(),
-        };
+        const registerForm = homePage.RegisterForm;
+        await registerForm.fill({ email });
+        await registerForm.fill(accountData);
 
-        await test.step('create new account via tooltip', async () => {
-            await header.clickAccountTooltip();
-            await header.clickCreateAccount();
-
-            const registerForm = homePage.RegisterForm;
-            await registerForm.fill({ email });
-            await registerForm.fill(accountData);
-
-            expect(await header.getUserName()).toBe(accountData.firstName);
-        });
-
-        await test.step('check registration data', async () => {
-            await header.clickWelcome();
-            await header.clickMyAccount();
-
-            expect(await myAccountSideBar.getUserName()).toBe(accountData.firstName);
-
-            await myAccountSideBar.menuButtonClick('myDetails');
-
-            const expextedAccountData = {
-                firstName: accountData.firstName,
-                lastName: accountData.lastName,
-                email,
-            };
-
-            expect(expextedAccountData).toEqual(await myAccountContent.getRegistrationData());
-        });
-
-        await test.step('change registration data and check result', async () => {});
-        await myAccountContent.buttonEditInformationClick();
-
-        const changedAccountData = {
-            firstName: faker.name.firstName(),
-            lastName: faker.name.lastName(),
-            email: email,
-        };
-
-        await myAccountContent.fillFormMyDetails(changedAccountData);
-        await myAccountContent.buttonSaveChangesClick();
-        await myAccountContent.buttonCloseChangesClick();
-
-        expect(changedAccountData).toEqual(await myAccountContent.getRegistrationData());
+        expect(await header.getUserName()).toBe(accountData.firstName);
     });
 });
