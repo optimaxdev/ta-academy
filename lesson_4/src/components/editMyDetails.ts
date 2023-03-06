3efc083dd688f3f38f572abf841193a624021f5b
Anna Razina <razina.start@gmail.com>
2023-03-06 18:30:39 +0400

    changes to variables in locators

--- lesson_4/src/components/editMyDetails.ts
+++ lesson_4/src/components/editMyDetails.ts
@@ -2,15 +2,15 @@ import { Component } from '@Core/component';
 
 export class EditMyDetails extends Component {
     protected LOCATOR = {
-        InputUserDetail: (name: string) => this.locator.locator(`//input[@placeholder="${name}"]`),
-        button: (name: string) => this.locator.locator(`//button[contains(., "${name}")]`),
+        InputUserDetail: (name: 'First Name' | 'Last Name' | 'Email Address') => this.locator.locator(`//input[@placeholder="${name}"]`),
+        button: (name: 'Save' | 'Close' | 'Cansel') => this.locator.locator(`//button[contains(., "${name}")]`),
     };
 
-    public async editUserDetails(nameInput: string, newUserDetails: string): Promise<void>  {
+    public async editUserDetails(nameInput: 'First Name' | 'Last Name' | 'Email Address', newUserDetails: string): Promise<void>  {
         await this.LOCATOR.InputUserDetail(`${nameInput}`).fill(`${newUserDetails}`);
     }
    
-    public async clickButton(name: string): Promise<void>  {
+    public async clickButton(name: 'Save' | 'Close' | 'Cansel'): Promise<void>  {
         await this.LOCATOR.button(name).click();
     }     
 }
--- lesson_4/src/components/myDetails.ts
+++ lesson_4/src/components/myDetails.ts
@@ -2,11 +2,11 @@ import { Component } from '@Core/component';
 
  export class MyDetails extends Component {
     protected LOCATOR = {
-        detailField: (name: string) => this.locator.locator(`//div[normalize-space(.)="${name}:"]/following-sibling::div`),
+        detailField: (name: 'First Name' | 'Last Name' | 'Email Address') => this.locator.locator(`//div[normalize-space(.)="${name}:"]/following-sibling::div`),
         buttonEditInformation: this.locator.locator('//button[contains(., "Edit Information")]'),
     };
 
-     public async getUserDetail(name: string):Promise<string | null> {
+     public async getUserDetail(name: 'First Name' | 'Last Name' | 'Email Address'):Promise<string | null> {
         return await this.LOCATOR.detailField(name).textContent();
     }
 
