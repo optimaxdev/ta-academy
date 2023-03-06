b4a1d8804c9f1dc088598c5bfc07fb8813e7ce98
Dmitry Shilovsky <wppbbh@gmail.com>
2023-03-06 09:53:15 +0400

    Homework6 done

--- lesson_4/src/components/header.ts
+++ lesson_4/src/components/header.ts
@@ -4,7 +4,6 @@ export class Header extends Component {
     protected LOCATORS = {
         accountTooltip: this.locator.locator('//button[contains(., "My Account")]'),
         createAccount: this.locator.locator('a', { hasText: 'Create Account' }),
-        myAccount: this.locator.locator('a', { hasText: 'My Account' }),
         welcome: this.locator.locator('//button[contains(., "Welcome,")]'),
     };
 
@@ -14,14 +13,6 @@ export class Header extends Component {
 
     public async clickCreateAccount(): Promise<void> {
         await this.LOCATORS.createAccount.click();
-    }
-
-    public async clickMyAccount(): Promise<void> {
-        await this.LOCATORS.myAccount.click();
-    }
-
-    public async clickWelcome(): Promise<void> {
-        await this.LOCATORS.welcome.click();
     }
 
     public async getUserName(): Promise<string | undefined> {
