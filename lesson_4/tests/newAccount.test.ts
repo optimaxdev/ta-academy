88b3757c38ecd73b50ae976f0637d5f58ebcfea1
Dmitry <ggbox@mail.ru>
2023-03-09 12:13:33 +0400

    init

---
+++ lesson_4/tests/newAccount.test.ts
@@ -0,0 +1,24 @@
+import { expect, test } from '@Test';
+import faker from 'faker';
+
+test.describe('create new account', () => {
+    const email = faker.internet.email();
+    const accountData = {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        password: 'Password123',
+    };
+
+    test('create new account via tooltip', async ({ homePage }) => {
+        await homePage.open();
+        const header = homePage.Header;
+        await header.clickAccountTooltip();
+        await header.clickCreateAccount();
+
+        const registerForm = homePage.RegisterForm;
+        await registerForm.fill({ email });
+        await registerForm.fill(accountData);
+
+        expect(await header.getUserName()).toBe(accountData.firstName);
+    });
+});
