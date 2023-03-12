db47075540f1e9c6329bf563b86c605e4d686e2e
Nikita K <114473212+churchkhelass@users.noreply.github.com>
2023-03-12 15:57:35 +0000

    practik_6

--- lesson_6/homework/__specs__/modules/components/cartPage/cartPage.ts
+++ lesson_6/homework/__specs__/modules/components/cartPage/cartPage.ts
@@ -1,8 +1,11 @@
 import { CartList } from '@Components/cartPage/cartList/cartList';
 import { Container } from '@Core/container';
+import { MyCartPopUp } from '@PopUp/myCartPopUp';
 
 const SELECTORS = {
     cartList: './/div[@class="cart__list"]',
+    buttonAddCartItem: `//button[text()="Add Cart Item"]`,
+    myCartPopUp: '//div[@data-testid="modal-inside"]',
 };
 
 export class CartPageContainer extends Container {
@@ -13,5 +16,11 @@ export class CartPageContainer extends Container {
     public async getCartList(): Promise<CartList> {
         const [cartList] = await document.waitForXpath(SELECTORS.cartList);
         return new CartList(cartList);
+    }
+
+    public async openNewCartPopUp(): Promise<MyCartPopUp> {
+        await document.clickByXpath(SELECTORS.buttonAddCartItem);
+        const popUpElement = document.$x(SELECTORS.myCartPopUp);
+        return new MyCartPopUp(popUpElement);
     }
 }
